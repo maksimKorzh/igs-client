2547851c6f0ec9dd4c409c49f163254060a805c9
Code Monkey King <freesoft.for.people@gmail.com>
2025-04-09 19:23:12 +0300

    fixed board sync bug, it needs improvements though

--- js/renderer.js
+++ js/renderer.js
@@ -6,7 +6,33 @@ const input = document.getElementById('input');
 window.telnetAPI.onData((data) => {
   output.textContent += data;
   output.scrollTop = output.scrollHeight;
-  doCommand(data);
+  // TODO: improve board parsing - switch games, userMove 
+  // Sync telnet and GUI board
+  if (data.includes('A B C D E F G')) {
+    clearBoard();
+    let boardStr = data.split('\n').slice(2, -2);
+    for (let l in boardStr) boardStr[l] = boardStr[l].slice(6, 43);
+    console.log(boardStr);
+    let rank = 0;
+    for (let row in boardStr) {
+      if (boardStr[row].length == 37) {
+        let file = 0;
+        let line = boardStr[row].replaceAll(' ', '');
+        for (let col of line) {
+          let sq = rank * size + file;
+          if (col == '>' || col == '<') {
+            if (col == '>') {
+              userMove = sq;
+              moveSound.play();
+            } continue;
+          }
+          if (col == '#') board[sq] = BLACK;
+          if (col == 'O') board[sq] = WHITE;
+          file++;
+        } rank++;
+      }
+    } drawBoard();
+  }
 });
 
 input.addEventListener('keydown', (e) => {
